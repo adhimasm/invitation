ae7d56ecb671c5fae2be3e6b77449266c068da1f
adhimasm <dhimaspianist@gmail.com>
2026-01-07 14:24:21 +0700

    Update and rename invi.js to list.js

--- list.js
+++ list.js
@@ -2,12 +2,12 @@
 // https://github.com/octokit/core.js#readme
 
 const octokit = new Octokit({
-  auth: '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OUR-TOKEN'
+  auth: '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'
 })
 
 await octokit.request('GET /repos/{owner}/{repo}/invitations', {
   owner: 'adhimasm',
-  repo: 'REPO',
+  repo: 'invitation',
   headers: {
     'X-GitHub-Api-Version': '2022-11-28'
   }
